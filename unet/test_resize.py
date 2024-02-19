da31601633a891a46db4e31fe1a7735a2ec25e62
Andrea Eusebi <andrea.eusebi@toyota-industries.eu>
2024-02-19 12:22:19 +0100

    Improved resizing test (antialasing should be disabled since it adds smoothing at edges).

--- unet/test_resize.py
+++ unet/test_resize.py
@@ -1,6 +1,5 @@
-import utils
-
 import logging
+import sys
 
 import torch
 from torchvision.datasets import Cityscapes
@@ -9,13 +8,36 @@ from torchvision.transforms.v2 import Resize
 
 import matplotlib.pyplot as plt
 
-def main():
+import utils
+
+H_DEFAULT = 128
+W_DEFAULT = 256
+
+def main(argv):
     logging.basicConfig(format="[test_resize.py][%(levelname)s]: %(message)s",
                         level=logging.INFO)
     logging.getLogger('matplotlib.font_manager').setLevel(logging.ERROR)
     logging.getLogger('PIL.PngImagePlugin').setLevel(logging.ERROR)
 
     logging.info("--- main() started. ---")
+
+    new_h = 0
+    new_w = 0
+
+    if len(argv) == 2:
+        logging.info(f"Received args: {argv[0]}, {argv[1]}")
+        new_h = int(argv[0])
+        new_w = int(argv[1])
+    elif len(argv) == 0:
+        logging.info(f"Using default height and widht: {H_DEFAULT}, {W_DEFAULT}")
+        new_h = H_DEFAULT
+        new_w = W_DEFAULT
+    else:
+        logging.critical(f"2 expected args. Received {len(argv)}.")
+        exit()
+
+    logging.debug(f"new_h: {new_h}")
+    logging.debug(f"new_w: {new_w}")
 
     ## Load training dataset
     train_data = Cityscapes(root="/home/andrea/datasets/cityscapes",
@@ -30,9 +52,11 @@ def main():
     utils.logTensorInfo(img_sample, "img_sample")
     utils.logTensorInfo(smnt_sample, "smnt_sample")
 
-    ## Resize image to a [3x572x572] as expected by Unet
-    resized_img = Resize(size=(572, 572), antialias=True)(img_sample)
-    resized_smnt = Resize(size=(572, 572), antialias=True)(smnt_sample)
+    ## Resize image to to the requested shape by Unet
+    ## Disable antialiasing since it smooths edges (it adds ficticious classes
+    ## at the borders of different labels)
+    resized_img = Resize(size=(new_h, new_w), antialias=False)(img_sample)
+    resized_smnt = Resize(size=(new_h, new_w), antialias=False)(smnt_sample)
 
     utils.logTensorInfo(resized_img, "resized_img")
     utils.logTensorInfo(resized_smnt, "resized_smnt")
@@ -60,4 +84,4 @@ def main():
     logging.info("--- main() completed. ---")
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
