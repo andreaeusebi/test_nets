de1a0206fabf5fa432cffb7db55b4242a093ab0a
Andrea Eusebi <andrea.eusebi@toyota-industries.eu>
2024-02-27 14:43:10 +0100

    Added functions for plotting transformed images

--- unet/utils.py
+++ unet/utils.py
@@ -1,4 +1,7 @@
 import logging
+import random
+from PIL import Image
+import matplotlib.pyplot as plt
 
 import torch
 from torchvision.transforms import functional
@@ -61,3 +64,36 @@ def labelToMask(label: torch.Tensor, palette: list) -> torch.Tensor:
     for i, color in enumerate(palette):
         mask[label == i] = torch.tensor(color)/255.0
     return mask.permute(2, 0, 1)
+
+def plot_transformed_images(image_paths, transform, n=3, seed=42):
+    """Plots a series of random images from image_paths.
+
+    Will open n image paths from image_paths, transform them
+    with transform and plot them side by side.
+
+    Args:
+        image_paths (list): List of target image paths. 
+        transform (PyTorch Transforms): Transforms to apply to images.
+        n (int, optional): Number of images to plot. Defaults to 3.
+        seed (int, optional): Random seed for the random generator. Defaults to 42.
+    """
+    random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(2, 1)
+            ax[0].imshow(f) 
+            ax[0].set_title(f"Original \nSize: {f.size}")
+            ax[0].axis("off")
+
+            # Transform and plot image
+            # Note: permute() will change shape of image to suit matplotlib 
+            # (PyTorch default is [C, H, W] but Matplotlib is [H, W, C])
+            transformed_image = transform(f).permute(1, 2, 0) 
+            ax[1].imshow(transformed_image) 
+            ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
+            ax[1].axis("off")
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+    plt.show()
