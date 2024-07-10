e76a2f9a6325f7a279d240371a324e6ba493d0c4
andreaeusebi <eusandre95@gmail.com>
2024-07-10 11:34:02 +0200

    General improvement to config file.

--- segformer_huggingface/config.py
+++ segformer_huggingface/config.py
@@ -11,28 +11,32 @@ project/
     models/         Where models state dictionaries and onnx files are saved.
     nets/           Contains networks implementation source code.
     dataset_utils/  Contains util scripts and files for datasets handling.
-    config.py
-    train.py
-    demo.py
-    demo.png
+    compile.py      Python script to compile a network and produce an .ONNX file.
+    config.py       Python file containing common parameters.
+    train.py        Python script for training a network.
+    demo.py         Python script for quick inference testing.
+    demo.png        Image used as default input for demo script.
 """
 
-##### ----- AUTOMATIC PARAMETERS ----- ######
+##### ----- AUTOMATIC PARAMETERS (DO NOT TOUCH) ----- ######
 
-DEVICE                  = "cuda" if torch.cuda.is_available() else "cpu"
 PROJECT_DIR             = str(Path(os.path.dirname(__file__)).absolute()) +  "/"
-
-################################################
-
-##### ----- PATHS TO FILES & DIRECTORIES ----- ######
-
 MODELS_DIR              = PROJECT_DIR + "models/"
 
 ################################################
 
-##### ----- GENERIC PARAMETERS ----- ######
+##### ----- COMMON PARAMETERS ----- ######
 
 LOGGING_LEVEL           = logging.INFO
+DEVICE                  = "cuda" if torch.cuda.is_available() else "cpu"
+IN_MODEL_NAME           = "nvidia/segformer-b0-finetuned-cityscapes-512-1024"
+H                       = 512
+W                       = 1024
+C                       = 3
+
+# DATASETS = ["TMHMI", "Cityscapes"]. Is a list of datasets to use, first one defines labels set and color palette.
+DATASETS                = ["TMHMI", "Cityscapes"]
+PALETTE                 = getPalette(DATASETS[0])
 
 ################################################
 
@@ -47,17 +51,7 @@ HF_DATASET              = "eusandre95/TMHMI_Semantic_Dataset"
 
 ################################################
 
-##### ----- INPUT PARAMETERS ----- ######
-
-# DATASETS = ["TMHMI", "Cityscapes"]. Is a list of datasets to use, first one defines labels set.
-DATASETS                = ["TMHMI", "Cityscapes"]
-PALETTE                 = getPalette(DATASETS[0])
-
-IN_MODEL_NAME           = "nvidia/segformer-b0-finetuned-cityscapes-512-1024"
-
-H                       = 512
-W                       = 1024
-C                       = 3
+##### ----- TRAINING PARAMETERS ----- ######
 
 EPOCHS                  = 50
 LEARNING_RATE           = 0.00006
@@ -70,7 +64,4 @@ BATCH_SIZE              = 8
 OUT_MODEL_NAME          = "240508-segformer-b2-finetuned-tmhmi-cs-512-1024-50ep-23labels-augm"
 OUT_ONNX_NAME           = "segformer-b0-finetuned-cityscapes-512-1024"
 
-################################################
-
-##### ----- TRAINING PARAMETERS ----- ######
 ################################################
